2453823ba4dcf649872cc5561b086a2b7b5012fd
Ally Boyd <alyssa.boyd85@gmail.com>
2017-02-15 12:58:37 -0600

    rendering to DOM, adding JS expressions into JSX

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// putting code between curly braces will cause it to be run as a JavaScript expression
 ReactDOM.render(
-  <h1>Hello world</h1>,
+  <h1>{5 + 5}</h1>,
   document.getElementById('container')
 );
 
+// first argument can be a variable
 var hello = <p>Hello World</p>;
+ReactDOM.render(hello, document.getElementById('second_container'));
+
+// be sure to close all tags, even ones considered self-closing
+var poem = (
+  <div>
+    <article>
+      Roses are red,
+      <br />
+      Violets are blue,
+      <br />
+      I am creative,
+      <br />
+      So this poem does not rhyme.
+    </article>
+  </div>
+);
+ReactDOM.render(poem, document.getElementById('third_container'));
